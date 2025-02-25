e76074a6af955d9d43ff0ba062894a409fe43b9c
5enthilbaliah <ternaryoperator.fork@gmail.com>
2025-02-25 09:47:21 -0600

    Dependency service modules

---
+++ lib-fw-all/lib-fw-modulez/IServiceModule.cs
@@ -0,0 +1,10 @@
+﻿namespace DistribuTe.Framework.ModuleZ;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public interface IServiceModule
+{
+    void Register(IServiceCollection services, IWebHostEnvironment environment, IConfiguration configuration);
+}
---
+++ lib-fw-all/lib-fw-modulez/Implementations/DependencyServiceModule.cs
@@ -0,0 +1,30 @@
+﻿namespace DistribuTe.Framework.ModuleZ.Implementations;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public abstract class DependencyServiceModule : IServiceModule
+{
+    private readonly List<IServiceModule> _modules = new();
+
+    protected void AddModule<TModule>()
+        where TModule : IServiceModule, new()
+    {
+        var module = new TModule();
+        _modules.Add(module);
+    }
+
+    protected abstract void RegisterCurrent(IServiceCollection services, IWebHostEnvironment environment,
+        IConfiguration configuration); 
+
+    public void Register(IServiceCollection services, IWebHostEnvironment environment, IConfiguration configuration)
+    {
+        foreach (var module in _modules)
+        {
+            module.Register(services, environment, configuration);
+        }
+        
+        RegisterCurrent(services, environment, configuration);
+    }
+}
