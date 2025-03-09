96eaf3f592af0da2019aa7a718c994661b58347b
5enthilbaliah <ternaryoperator.fork@gmail.com>
2025-03-09 17:19:07 -0500

    Handle validation problem

--- lib-fw-all/lib-fw-odata/Controllers/DistribuTeController.cs
+++ lib-fw-all/lib-fw-odata/Controllers/DistribuTeController.cs
@@ -3,22 +3,46 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 [ApiController]
 public class DistribuTeController : ControllerBase
 {
-    protected IActionResult Problem(List<Error> errors)
+    private IActionResult ValidationProblem(List<Error> errors)
     {
-        var firstError = errors.First();
-
-        var statusCode = firstError.Type switch
+        var modelStateDict = new ModelStateDictionary();
+        foreach (var error in errors)
+        {
+            modelStateDict.AddModelError(error.Code, error.Description);
+        }
+        return ValidationProblem(modelStateDict);
+    }
+    
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.NotFound => StatusCodes.Status404NotFound,
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             _ => StatusCodes.Status500InternalServerError,
         };
+
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+    
+    protected IActionResult Problem(List<Error> errors)
+    {
+        if (errors.Count == 0)
+        {
+            return Problem();
+        }
         
-        return Problem(statusCode: statusCode, title: firstError.Description);
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Problem(errors.First());
     }
 }
